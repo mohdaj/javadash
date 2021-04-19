c45e3d7dec7e407b2490cffa0efc171f87797cb8
mohadaj <engmoh24@gmail.com>
2021-04-19 17:28:03 +0300

    read file

--- src/main/java/sa/alrajhi/jbl/serverutilization/readjson.java
+++ src/main/java/sa/alrajhi/jbl/serverutilization/readjson.java
@@ -1,10 +1,22 @@
 package sa.alrajhi.jbl.serverutilization;
 
-import java.io.FileNotFoundException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.*;
+import java.util.List;
 
 public class readjson {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper =new ObjectMapper();
+        InputStream inputStream =new FileInputStream(new File("C:/Users/engmo/Desktop/data.json"));
+        TypeReference<List<object>> listTypeReference= new TypeReference<List<object>>(){};
+        List<object>objects =mapper.readValue(inputStream,listTypeReference);
+        for(object p : objects){
+            System.out.println(p.getEnvironment() + p.getUser().getAdmin());
+        }
+
 
 
 
